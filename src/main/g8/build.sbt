38e336d3bea7590bb7d69e96d3f75e9d464f1e24
tomaswyz <jackywyz@126.com>
2013-10-30 21:45:08 +0800

    Upgrade some jars's version and update the spray routing and servlet demos

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -2,19 +2,20 @@ name :="spray-demo"
 
 version :="0.1"
 
-scalaVersion :="2.9.2"
+scalaVersion :="2.10.3"
 
 seq(webSettings :_*)
 
+conflictWarning := ConflictWarning.disable
 
 resolvers +="spray repo" at "http://repo.spray.io/"
 
+libraryDependencies ++=Seq(
+"org.mortbay.jetty" % "jetty" % "6.1.26" % "container",
+"ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime",
+"io.spray" %  "spray-routing"    % "1.2-RC2"   % "compile",
+"io.spray" %  "spray-can"    % "1.2-RC2"   % "compile",
+"com.typesafe.akka" %% "akka-actor" % "2.2.3" ,
+"com.chuusai" % "shapeless_2.10.2" % "2.0.0-M1"
 
-libraryDependencies ++=Seq("com.typesafe.akka" % "akka-actor" % "2.0.3",
-    "com.typesafe.akka" % "akka-slf4j" % "2.0.3",
-     "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-     "cc.spray"%  "spray-server"    % "1.0-M2.2"   % "compile",
-     "org.specs2" %% "specs2" % "1.12.2" % "test",
-     "org.eclipse.jetty" % "jetty-webapp" % "8.1.5.v20120716" % "container",
-     "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container" artifacts (Artifact("javax.servlet", "jar", "jar"))
 )
--- src/main/g8/src/main/scala/com/example/HelloService.scala
+++ src/main/g8/src/main/scala/com/example/HelloService.scala
@@ -1,51 +1,96 @@
 package com.example
 
-import cc.spray._
-import http.MediaTypes._
-import akka.util.duration._
+import scala.concurrent.duration._
+import akka.io.Tcp
+import akka.actor._
+import spray.http._
+import MediaTypes._
+import HttpMethods._
 
-trait HelloService extends Directives {
-  
-  val simpleService = {
-    path("") {
-      get {
-        respondWithMediaType(`text/html`) {
-          _.complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>spray</i>!</h1>
-                <p>Defined resources:</p>
-                <ul>
-                  <li><a href="/ping">/ping</a></li>
-                  <li><a href="/timeout">/timeout</a></li>
-                  <li><a href="/2nd">/2nd</a></li>
-                </ul>
-              </body>
-            </html>
-          }
+class HelloService extends Actor with ActorLogging {
+
+  def receive = {
+    case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+      sender ! index
+
+    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+      sender ! HttpResponse(entity = "PONG!")
+
+    case HttpRequest(GET, Uri.Path("/stream"), _, _, _) =>
+      val client = sender // since the Props creator is executed asyncly we need to save the sender ref
+      context.actorOf(Props(new Streamer(client, 20)))
+
+    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+      sender ! HttpResponse(entity = "About to throw an exception in the request handling actor, " +
+        "which triggers an actor restart")
+      sys.error("BOOM!")
+
+    case HttpRequest(GET, Uri.Path("/timeout"), _, _, _) =>
+      log.info("Dropping request, triggering a timeout")
+
+    case HttpRequest(GET, Uri.Path("/changetimeout"), _, _, _) =>
+      sender ! SetRequestTimeout(60.seconds)
+      log.info("Changing timeout initially set by 'spray.servlet.request-timeout' property and triggering timeout")
+
+    case HttpRequest(GET, Uri.Path("/timeout/timeout"), _, _, _) =>
+      log.info("Dropping request, triggering a timeout")
+
+    case _: HttpRequest => sender ! HttpResponse(404, "Unknown resource!")
+
+    case Timedout(HttpRequest(_, Uri.Path("/timeout/timeout"), _, _, _)) =>
+      log.info("Dropping Timeout message")
+
+    case Timedout(request: HttpRequest) =>
+      sender ! HttpResponse(500, "The " + request.method + " request to '" + request.uri + "' has timed out...")
+  }
+
+  ////////////// helpers //////////////
+
+  lazy val index = HttpResponse(
+    entity = HttpEntity(`text/html`,
+      <html>
+        <body>
+          <h1>Say hello to <i>spray-servlet</i>!</h1>
+          <p>Defined resources:</p>
+          <ul>
+            <li><a href="/ping">/ping</a></li>
+            <li><a href="/stream">/stream</a></li>
+            <li><a href="/crash">/crash</a></li>
+            <li><a href="/timeout">/timeout</a></li>
+            <li><a href="/timeout/timeout">/timeout/timeout</a></li>
+          </ul>
+        </body>
+      </html>.toString
+    )
+  )
+
+  // simple case class whose instances we use as send confirmation message for streaming chunks
+  case class Ok(remaining: Int)
+
+  class Streamer(client: ActorRef, count: Int) extends Actor with ActorLogging {
+    log.debug("Starting streaming response ...")
+
+    // we use the successful sending of a chunk as trigger for scheduling the next chunk
+    client ! ChunkedResponseStart(HttpResponse(entity = " " * 2048)).withAck(Ok(count))
+
+    def receive = {
+      case Ok(0) =>
+        log.info("Finalizing response stream ...")
+        client ! MessageChunk("\nStopped...")
+        client ! ChunkedMessageEnd
+        context.stop(self)
+
+      case Ok(remaining) =>
+        log.info("Sending response chunk ...")
+        import context.dispatcher
+        context.system.scheduler.scheduleOnce(100.millis) {
+          client ! MessageChunk(DateTime.now.toIsoDateTimeString + ", ").withAck(Ok(remaining - 1))
         }
-      }
-    } ~
-    path("ping") {
-      content(as[Option[String]]) { body =>
-        completeWith("PONG! " + body.getOrElse(""))
-      }
-    } ~
-    path("timeout") {
-      get { ctx =>
-        actorSystem.scheduler.scheduleOnce(1500.millis) {
-          ctx.complete("Too late!")
-        }
-      }
+
+      case ev: Tcp.ConnectionClosed =>
+        log.info("Canceling response stream due to {} ...", ev)
+        context.stop(self)
     }
   }
 
-  val secondService = {
-    path("2nd") {
-      get {
-        completeWith("A reply from a second service!")
-      }
-    }
-  }
-  
 }
